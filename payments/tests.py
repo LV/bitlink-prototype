28e91b0c87ee233904fc8601c0f19ecfb85bbabc
Luis Victoria <x@lambda.lv>
2022-07-19 13:05:27 -0700

    Create Customer Model

--- payments/tests.py
+++ payments/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Customer
+
+
+class CustomerTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.customer = Customer.objects.create(
+            name="Bob Bobson",
+            email="lavp@pm.me",
+        )
+
+    def test_customer_content(self):
+        self.assertEqual(self.customer.name, "Bob Bobson")
+        self.assertEqual(self.customer.email, "lavp@pm.me")
