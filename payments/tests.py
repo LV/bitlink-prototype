6c34293361c5028e27ee67ab2dd062e77ef0fb92
Luis Victoria <x@lambda.lv>
2022-07-19 13:42:36 -0700

    Create customer_list View

--- payments/tests.py
+++ payments/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 
 from .models import Customer
 
@@ -14,3 +15,9 @@ class CustomerTests(TestCase):
     def test_customer_content(self):
         self.assertEqual(self.customer.name, "Bob Bobson")
         self.assertEqual(self.customer.email, "lavp@pm.me")
+
+    def test_customer_listview(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)     # Status code 200 = Success
+        self.assertContains(response, "Bob Bobson")
+        self.assertTemplateUsed(response, "payments/customer_list.html")
