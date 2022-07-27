9ce93cf94668f60b65a8ee31718daede966a8c27
Yan <yyzhang@student.ubc.ca>
2022-07-27 16:08:44 -0700

    install material ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Button from '@mui/material/Button'
+import Switch from '@mui/material/Switch'
+
 
 function App() {
   return (
@@ -8,6 +11,13 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Bitlink
+          <Button
+            color="primary"
+            variant="contained"
+          > HELLO </Button>
+          <Switch
+            color="error"
+          />
         </p>
       </header>
     </div>
