dcf2df8750fcace55c50675017ed053bd19f6b9f
Yan <yyzhang@student.ubc.ca>
2022-07-30 15:26:03 -0700

    Create main page buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,46 @@
 import logo from "./images/logo.png";
 import "./App.css";
 import Button from '@mui/material/Button'
-import Switch from '@mui/material/Switch'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#3DFC4B',
+    },
+    secondary: {
+      main: '#EEEEEE',
+    },
+  },
+})
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo}  className="App-logo" alt="logo"/>
         <p>
           Bitlink
         </p>
-        <Button
-            color="primary"
-          > HELLO </Button>
-          <Switch
-            color="primary"
-          />
+        <div>
+        <ThemeProvider theme={theme}>
+          <Button
+              color="secondary"
+              variant="contained"
+              style={{marginRight: '30px'}}
+            > Customer Dashboard </Button>
+          <Button
+              color="secondary"
+              variant="contained"
+          > Merchant Dashboard </Button>
+          <Button
+              color="secondary"
+              variant="contained"
+              style={{marginLeft: '30px'}}
+          > Company Dashboard </Button>
+          </ThemeProvider>
+        </div>
       </header>
     </div>
   );
---
+++ frontend/src/Components/Dashboard.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Dashboard(props) { 
+  const { type } = props
+  
+  return (
+    <>${type}</>
+  );
+}
+export default Dashboard;
