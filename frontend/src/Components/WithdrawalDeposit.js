21265dfbdcb4939bfcea45d4df1ff76bd59c8c95
Yan <yyzhang@student.ubc.ca>
2022-08-01 13:53:42 -0700

    Get withdrawal and deposit amounts

--- frontend/src/Components/WithdrawalDeposit.js
+++ frontend/src/Components/WithdrawalDeposit.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import TopBar from "./TopBar";
 import { TextField, Button, FormControl } from "@mui/material";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
@@ -11,30 +11,37 @@ const theme = createTheme({
     },
   })
 
-function handleSubmit(e) {
-    console.log(e)
-}
-
 export default function WithdrawalDeposit(props) {
     const { isDeposit } = props;
 
+    const [value, setValue] = useState(0)
+
     const type = isDeposit ? "Deposit" : "Withdrawal"
+
+    function handleSubmit() {
+      console.log(value)
+  }
 
     return (
         <>
             <TopBar/>
             <FormControl
-                style={{display: 'flex', alignItems: 'center'}}>
+                style={{height: 400, display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
             <h3>Enter in amount to {type.toLowerCase()}:</h3>
-            <TextField id="outlined-basic" label="Amount" variant="outlined" />
+            <TextField 
+              id="outlined-basic" 
+              label="Amount" 
+              variant="outlined"
+              onChange={e => setValue(e.target.value)}
+              />
             <ThemeProvider theme={theme}>
                 <Button
                     color="primary"
-                    type="number"
                     variant="contained"
                     style={{margin: 20}}
-                    onClick={e => handleSubmit(e)}
-                    > Submit </Button>
+                    onClick={handleSubmit}
+                    > Submit 
+                </Button>
             </ThemeProvider>
             </FormControl>
         </>
