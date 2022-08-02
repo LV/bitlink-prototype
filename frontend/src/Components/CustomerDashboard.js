0b2eed401945f42a4d0391a28d99db45add87d58
Yan <yyzhang@student.ubc.ca>
2022-08-02 15:28:39 -0700

    Update BTC balance based on deposit/withdraw amt

--- frontend/src/Components/CustomerDashboard.js
+++ frontend/src/Components/CustomerDashboard.js
@@ -1,8 +1,8 @@
 import { Button, FormControl } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import TopBar from "./TopBar";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import {useNavigate} from 'react-router-dom';
+import WithdrawalDeposit from "./WithdrawalDeposit";
 
 const theme = createTheme({
   palette: {
@@ -12,35 +12,60 @@ const theme = createTheme({
   },
 })
 
-function CustomerDashboard() {
+export default function CustomerDashboard() {
 
-  const navigate = useNavigate();
+  const [showDeposit, setShowDeposit] = useState(false)
+  const [showWithdrawal, setShowWithdrawal] = useState(false)
+  const [balance, setBalance] = useState(10)
+
+  function handleDeposit() {
+    setShowDeposit(prev => !prev);
+  }
+
+  function handleWithdrawal() {
+    setShowWithdrawal(prev => !prev);
+  }
+
+  function sendDeposit(updatedBalance) {
+    const newBalance = balance + Number(updatedBalance)
+    setBalance(newBalance);
+  };
+
+  function sendWithdrawal(updatedBalance) {
+    const newBalance = balance - Number(updatedBalance)
+
+    if (newBalance < 0) {
+      alert("Negative balance, please try again.")
+    } else {
+      setBalance(newBalance);
+    }
+  };
 
   return (
     <>
     <TopBar/>
     <FormControl
       style={{height: 600, display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-      <h3>BTC Balance: 0.230123</h3>
+      <h3>BTC Balance: {balance}</h3>
       <div style={{height: 200, width: 400, display: 'flex', alignItems: 'center', justifyContent: 'space-around'}}>
         <ThemeProvider theme={theme}>
           <Button
             color="primary"
             variant="contained"
-            onClick={() => navigate("/deposit")}
+            onClick={handleDeposit}
             > Deposit 
           </Button>
           <Button
             color="primary"
             variant="contained"
-            onClick={() => navigate("/withdrawal")}
+            onClick={handleWithdrawal}
             > Withdrawal 
           </Button>
         </ThemeProvider>
       </div>
+      {showDeposit && <WithdrawalDeposit isDeposit={true} sendDeposit={sendDeposit}/>}
+      {showWithdrawal && <WithdrawalDeposit isDeposit={false} sendWithdrawal={sendWithdrawal}/>}
     </FormControl>
   </>
   );
 }
-
-export default CustomerDashboard;
