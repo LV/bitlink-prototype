524a3bf5538218b3c395e1e13404c8b719450b19
Luis Victoria <x@lambda.lv>
2022-08-02 20:48:33 -0700

    Change TopBar Logo

--- frontend/src/Components/TopBar.js
+++ frontend/src/Components/TopBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button } from "@mui/material";
 import {useNavigate} from 'react-router-dom';
-import logo from './logocropped.png';
+import logo from './logo.png';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const theme = createTheme({
