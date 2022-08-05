c83325464322ea4d39379f3009f6d0088cd13010
Yan <yyzhang@student.ubc.ca>
2022-08-05 14:25:32 -0700

    Add input fields to OrderDetails

--- frontend/src/Components/OrderDetails.js
+++ frontend/src/Components/OrderDetails.js
@@ -1,74 +1,151 @@
-import React from "react";
-import { AgGridReact } from 'ag-grid-react';
-import { List, ListItem, ListItemText, createTheme, ThemeProvider } from '@mui/material';
+import React, { useState, useEffect } from "react";
+import { AgGridReact } from "ag-grid-react";
+import {
+  createTheme,
+  ThemeProvider,
+  FormControl,
+  InputLabel,
+  Input,
+  Button,
+} from "@mui/material";
 import LoadingButtonsTransition from "./AnimatedButton";
+import axios from "axios";
+import useGetCustomerDataById from "../Hooks/useGetCustomerDataById";
+import useGetMerchantDataById from "../Hooks/useGetMerchantDataById";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#EEEEEE",
+    },
+  },
+});
 
 export default function OrderDetails() {
+  const [item, setItem] = useState("");
+  const [type, setType] = useState("");
+  const [price, setPrice] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [addItem, setAddItem] = useState({
+    item: "",
+    type: "",
+    price: "",
+    quantity: "",
+  });
 
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: '#EEEEEE',
-      },
-    },
-  })
+  const merchantId = 3; //hardcode
+  const customerId = 4; //hardcode
 
-  const rowData = [
-    { item: 'testName', quantity: '1', price: '$100'},
-    { item: 'testName', quantity: '1', price: '$100'},
-    { item: 'testName', quantity: '1', price: '$100'}
-  ];
+  const customerData = useGetCustomerDataById(customerId);
+  const merchantData = useGetMerchantDataById(merchantId);
 
   const columnDefs = [
-    {field: 'item'},
-    {field: 'quantity'},
-    {field: 'price'}
+    { field: "item" },
+    { field: "type" },
+    { field: "quantity" },
+    { field: "price" },
   ];
 
+  const rowData = [];
 
   const current = new Date();
-  const date = current.toLocaleDateString()
-  const time = current.toLocaleTimeString()
+  const date = current.toLocaleDateString();
+  const time = current.toLocaleTimeString();
 
-    return (
-      <>
-          <h3 style={{marginLeft: 22}}>Tesla</h3>
-          <h5 style={{marginLeft: 22}}>Order ID: 00000001</h5>
-          <h5 style={{marginLeft: 22}}>Date: {date} {time} </h5>
-          <div
-          style = {{display: 'flex', alignItems: 'space-between'}}
-          >          
-            <div className='ag-theme-alpine' style={{ width: 610, height: 500, marginLeft: 20}}>
-            <AgGridReact
-              rowData={rowData}
-              columnDefs={columnDefs}
-            />
-           </div>
-          <div
-            style = {{marginLeft: 350}}
-          >
-          <List sx={{width: '100%', maxWidth: 360 }}>
-              <ListItem>
-                <ListItemText primary={`Customer First Last Name`} />
-              </ListItem>
-              <ListItem>
-                <ListItemText primary={`customer@email.com`} />
-              </ListItem>
-          </List>
+  function handleAddItem() {
+    if (!item || !type || !price || !quantity) {
+      alert("Please enter in all fields.");
+    } else {
+      setAddItem({ item, type, price, quantity });
+    }
+  }
+
+  return (
+    <>
+      <div style={{ marginLeft: 22, marginTop: 20 }}>
+        <h3>{merchantData ? merchantData.name : ""}</h3>
+        <h5>Order ID: 00000001</h5>
+        <h5>
+          Date: {date} {time}{" "}
+        </h5>
+      </div>
+      <div
+        style={{
+          marginLeft: 20,
+          display: "flex",
+          alignItems: "flex-end",
+        }}
+      >
+        <FormControl variant="standard" style={{ width: 200, marginLeft: 2 }}>
+          <InputLabel>Item</InputLabel>
+          <Input
+            autoComplete="off"
+            type="text"
+            onChange={(e) => setItem(e.target.value)}
+          />
+        </FormControl>
+        <FormControl variant="standard" style={{ marginLeft: 20, width: 200 }}>
+          <InputLabel>Item Type</InputLabel>
+          <Input
+            autoComplete="off"
+            type="text"
+            onChange={(e) => setType(e.target.value)}
+          />
+        </FormControl>
+        <FormControl variant="standard" style={{ marginLeft: 20, width: 125 }}>
+          <InputLabel>Price</InputLabel>
+          <Input
+            autoComplete="off"
+            type="text"
+            onChange={(e) => setPrice(e.target.value)}
+          />
+        </FormControl>
+        <FormControl variant="standard" style={{ marginLeft: 20, width: 75 }}>
+          <InputLabel>Quantity</InputLabel>
+          <Input
+            autoComplete="off"
+            type="text"
+            onChange={(e) => setQuantity(e.target.value)}
+          />
+        </FormControl>
+        <div style={{ marginLeft: 50 }}>
           <ThemeProvider theme={theme}>
-            {/* <Button
-                color="primary"
-                variant="contained"
-                style={{marginLeft: '15px'}}
-                onClick={() => alert("Success!!")}
-            > BitLink Checkout</Button> */}
-            <LoadingButtonsTransition />
-            
+            <Button
+              color="primary"
+              variant="contained"
+              size="small"
+              onClick={handleAddItem}
+            >
+              {" "}
+              Add to cart
+            </Button>
           </ThemeProvider>
         </div>
       </div>
-      <h4 style={{marginLeft: 22}}>Order Total (USD): $300</h4>
-      <h4 style={{marginLeft: 22}}>Order Total (BTC): ₿0.000043</h4> 
+      <div style={{ display: "flex" }}>
+        <div
+          className="ag-theme-alpine"
+          style={{
+            width: 820,
+            height: 400,
+            marginLeft: 20,
+            marginTop: 30,
+          }}
+        >
+          <AgGridReact rowData={rowData} columnDefs={columnDefs} />
+        </div>
+        <div style={{ marginLeft: 350 }}>
+          <div style={{ marginBottom: 20 }}>
+            <h5>{customerData ? customerData.name : ""}</h5>
+            <p>{customerData ? customerData.email : ""}</p>
+          </div>
+          <ThemeProvider theme={theme}>
+            <LoadingButtonsTransition />
+          </ThemeProvider>
+        </div>
+      </div>
+      <h4 style={{ marginLeft: 22 }}>Order Total (USD): $300</h4>
+      <h4 style={{ marginLeft: 22 }}>Order Total (BTC): ₿0.000043</h4>
     </>
   );
 }
